07038f6312871f80674a9197b5f52b691c7e6401
Teijo Laine <teijo@aropupu.fi>
2013-02-16 20:10:05 +0200

    Store state in a single property and make it mutable

--- jquery.group.js
+++ jquery.group.js
@@ -1,13 +1,12 @@
-function makeStandings(pairs) {
-  var participants = pairs.pluck('home').union(pairs.pluck('away').value())
+function makeStandings(participants, pairs) {
   return participants.map(function(it) {
     var matches = pairs
-      .filter(function(match) { return match.home === it || match.away === it })
+      .filter(function(match) { return match[0].name === it || match[1].name === it })
       .map(function(match) {
-        if (match.home === it)
-          return { ownScore: match.homeScore, opponentScore: match.awayScore }
+        if (match[0].name === it)
+          return { ownScore: match[0].score, opponentScore: match[1].score }
         else
-          return { ownScore: match.awayScore, opponentScore: match.homeScore }
+          return { ownScore: match[1].score, opponentScore: match[0].score }
       })
     var wins = matches.filter(function(match) { return (match.ownScore > match.opponentScore) }).size()
     var losses = matches.filter(function(match) { return (match.ownScore < match.opponentScore) }).size()
@@ -41,16 +40,25 @@ $(function() {
       +'</colgroup>'
       +'<tr><th>Name</th><th>W</th><th>L</th><th>T</th><th>P</tr>'
       +'{{#each this}}'
-      +'<tr><td><input value="{{name}}" /></td><td>{{wins}}</td><td>{{losses}}</td><td>{{ties}}</td><td>{{points}}</td></tr>'
+      +'<tr><td><input class=="name" value="{{name}}" /></td><td>{{wins}}</td><td>{{losses}}</td><td>{{ties}}</td><td>{{points}}</td></tr>'
       +'{{/each}}'
       +'</table>'
+      +'<input class="add" value="{{name}}" /><input type="submit" value="Add" />'
       +'</div>')
     var roundsMarkup = Handlebars.compile(
       '<div class="rounds"></div>')
     var unassignedMarkup = Handlebars.compile(
       '<div class="unassigned"></div>')
     return {
-      standings: function(participants) { return $(standingsMarkup(participants.value())) },
+      standings: function(participantStream, participants) {
+        var markup = $(standingsMarkup(participants.value()))
+        markup.find('input.add').asEventStream('change')
+          .map('.target')
+          .map($)
+          .map(function(el) { return el.val() })
+          .onValue(function(value) { participantStream.push(value) })
+        return markup
+      },
       rounds: $(roundsMarkup()),
       unassigned: $(unassignedMarkup())
     }
@@ -94,7 +102,7 @@ $(function() {
       +'</div>')
 
     return {
-      create: function(match) {
+      create: function(resultStream, match) {
         return new function() {
           var that = this
           match.id = ++id
@@ -104,14 +112,16 @@ $(function() {
           var scoreChanges = markup.find('input').asEventStream('change').map('.target').map($)
 
           this.property = Bacon.combineTemplate({
-            home: match.home,
             homeScore: scoreChanges.filter('.hasClass', 'home')
               .map(function(it) { return it.val() })
               .toProperty(match.homeScore),
-            away: match.away,
             awayScore: scoreChanges.filter('.hasClass', 'away')
               .map(function(it) { return it.val() })
               .toProperty(match.awayScore)
+          })
+
+          this.property.onValue(function(result) {
+            resultStream.push([ { name: match.home, score: result.homeScore }, { name: match.away, score: result.awayScore } ])
           })
 
           markup.asEventStream('dragstart').map(".originalEvent").onValue(function(ev) {
@@ -129,20 +139,71 @@ $(function() {
     }
   })()
 
+  var matchStream = new Bacon.Bus()
+  var participantStream = new Bacon.Bus()
+  var renameStream = new Bacon.Bus()
+  var resultStream = new Bacon.Bus()
+
+  var matchProp = matchStream.toProperty({ participants: _([]), matches: _([]) })
+  var participantAdds = matchProp.sampledBy(participantStream, function(propertyValue, streamValue) {
+    if (propertyValue.participants.size() > 0) {
+      var newMatches = propertyValue.participants.map(function(it) {
+        return [ { name: it, score: null }, { name: streamValue, score: null } ]
+      })
+      propertyValue.matches = propertyValue.matches.union(newMatches.value())
+    }
+    propertyValue.participants.push(streamValue)
+    return propertyValue
+  })
+  var resultUpdates = matchProp.sampledBy(resultStream, function(propertyValue, streamValue) {
+    propertyValue.matches = propertyValue.matches.map(function(it) {
+      if (it[0].name === streamValue[0].name && it[1].name === streamValue[1].name) {
+        it[0].score = streamValue[0].score
+        it[1].score = streamValue[1].score
+      } else if (it[0].name === streamValue[1].name && it[1].name === streamValue[0].name) {
+        it[0].score = streamValue[1].score
+        it[1].score = streamValue[0].score
+      }
+      return it
+    })
+    return propertyValue
+  })
+  var participantRenames = matchProp.sampledBy(renameStream, function(propertyValue, streamValue) {
+    propertyValue.participants = propertyValue.participants.map(function(it) {
+      if (it === streamValue.from)
+        return streamValue.to
+      else
+        return it
+    })
+    propertyValue.matches = propertyValue.matches.map(function(it) {
+      if (it[0].name === streamValue.from) {
+        it[0].name = streamValue.to
+      } else if (it[1].name === streamValue.from) {
+        it[1].name = streamValue.to
+      }
+      return it
+    })
+    return propertyValue
+  })
+
+  var result = Bacon.mergeAll([participantAdds, resultUpdates, participantRenames])
+  result.throttle(10).onValue(function() { console.log('New state created'); console.log(arguments) })
+
+  participants.each(function(it) { participantStream.push(it) })
+
+  //renameStream.push({ from: 'b', to: 'e' })
+
+  resultUpdates.throttle(10).onValue(function(state) {
+    $('.standings').replaceWith(templates.standings(participantStream, makeStandings(state.participants, state.matches)))
+  })
+
   $('<div class="standings"></div>').appendTo($container)
   var rounds = templates.rounds.appendTo($container)
   _([1, 2, 3, 4]).each(function(it) {
     rounds.append(Round.create(it).markup)
   })
   var unassigned = templates.unassigned.appendTo($container)
-  var properties = []
   pairs.each(function(it) {
-    var match = Match.create(it)
-    properties.push(match.property)
-    unassigned.append(match.markup)
-  })
-  var state = Bacon.combineAsArray(properties)
-  state.onValue(function(val) {
-    $('.standings').replaceWith(templates.standings(makeStandings(_(val))))
+    unassigned.append(Match.create(resultStream, it).markup)
   })
 })
